c09afc25a14c746679ac929fc14d4ca51b94eed4
Georgios Voulgaris <gvsam7@yahoo.com>
2022-08-15 14:28:04 +0100

    debug save_table

--- utilities/utils.py
+++ utilities/utils.py
@@ -172,7 +172,8 @@ def save_table(loader, model, table_name, device, folder="saved_images/"):
 
         plt.figure(figsize=(10, 10))
         plt.axis("off")
-        plt.imshow(preds.permute(1, 2, 3, 0).detach().cpu()[:, :, :, 0])
+        # plt.imshow(preds.permute(1, 2, 0).detach().cpu()[:, :, 0])
+        plt.imshow(preds.permute(2, 3, 0, 1).detach().cpu()[:, :, 0])
         plt.savefig("predicted_mask.jpg")
         plt.close()
 
