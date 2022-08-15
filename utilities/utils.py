f28e894fb5e1683eaa1bebcaab2c66dbf039ef59
Georgios Voulgaris <gvsam7@yahoo.com>
2022-08-15 15:26:12 +0100

    debug save_table

--- utilities/utils.py
+++ utilities/utils.py
@@ -117,7 +117,7 @@ def save_table(loader, model, table_name, device):
 
         plt.figure(figsize=(10, 10))
         plt.axis("off")
-        plt.imshow(im.permute(1, 2, 0).detach().cpu()[:, :, 0])
+        plt.imshow(im[0].permute(1, 2, 0).detach().cpu()[:, :, 0])
         plt.savefig("original_image.jpg")
         plt.close()
 
