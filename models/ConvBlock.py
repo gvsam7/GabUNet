9c6d4ff07c7c3ce71ed58ce1e9f838e8f8e1eb66
Georgios Voulgaris <gvsam7@yahoo.com>
2022-08-24 12:23:31 +0100

    added gabor conv on vertical and horizontal conv

--- models/ConvBlock.py
+++ models/ConvBlock.py
@@ -82,8 +82,8 @@ class GACBlock(nn.Module):
     def __init__(self, in_planes, out_planes):
         super(GACBlock, self).__init__()
         self.square = GaborConv2d(in_planes, out_planes, kernel_size=3, padding=1, stride=1)
-        self.cross_ver = nn.Conv2d(in_planes, out_planes, kernel_size=(1, 3), padding=(0, 1), stride=1)
-        self.cross_hor = nn.Conv2d(in_planes, out_planes, kernel_size=(3, 1), padding=(1, 0), stride=1)
+        self.cross_ver = GaborConv2d(in_planes, out_planes, kernel_size=(1, 3), padding=(0, 1), stride=1)
+        self.cross_hor = GaborConv2d(in_planes, out_planes, kernel_size=(3, 1), padding=(1, 0), stride=1)
         self.bn = nn.BatchNorm2d(out_planes)
         self.relu = nn.ReLU(True)
 
