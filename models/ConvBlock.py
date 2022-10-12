3b724036be05fb6c18ec72d419907f3749fc27b3
Georgios Voulgaris <gvsam7@yahoo.com>
2022-10-12 17:38:55 +0100

    debug Asymmetric Convolution UNet

--- models/ConvBlock.py
+++ models/ConvBlock.py
@@ -78,9 +78,9 @@ class ChannelAttention(nn.Module):
 class ACBlock(nn.Module):
     def __init__(self, in_planes, out_planes):
         super(ACBlock, self).__init__()
-        self.square = nn.Conv2d(in_planes, out_planes, kernel_size=3, padding=1, stride=1)
-        self.cross_ver = nn.Conv2d(in_planes, out_planes, kernel_size=(1, 3), padding=(0, 1), stride=1)
-        self.cross_hor = nn.Conv2d(in_planes, out_planes, kernel_size=(3, 1), padding=(1, 0), stride=1)
+        self.square = nn.Conv2d(in_planes, out_planes, kernel_size=3, padding=1, stride=1, bias=False)
+        self.cross_ver = nn.Conv2d(in_planes, out_planes, kernel_size=(1, 3), padding=(0, 1), stride=1, bias=False)
+        self.cross_hor = nn.Conv2d(in_planes, out_planes, kernel_size=(3, 1), padding=(1, 0), stride=1, bias=False)
         self.bn = nn.BatchNorm2d(out_planes)
         self.relu = nn.ReLU(True)
 
