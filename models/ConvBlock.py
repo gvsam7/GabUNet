89ba5fdccc3b5645b3bc3c2bdfb3fdedea575afc
Georgios Voulgaris <gvsam7@yahoo.com>
2023-04-06 18:23:28 +0100

    Added dilated convolutional layer

--- models/ConvBlock.py
+++ models/ConvBlock.py
@@ -4,6 +4,25 @@ from models.GaborLayer import GaborConv2d
 from models.MixPool import MixPool
 
 
+class DACBlock(nn.Module):
+    def __init__(self, in_planes, out_planes):
+        super(DACBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, padding='same', stride=1, dilation=1)
+        self.conv3 = nn.Conv2d(in_planes, out_planes, kernel_size=3, padding='same', stride=1, dilation=3)
+        self.conv6 = nn.Conv2d(in_planes, out_planes, kernel_size=3, padding='same', stride=1, dilation=6)
+        self.conv9 = nn.Conv2d(in_planes, out_planes, kernel_size=3, padding='same', stride=1, dilation=9)
+        self.bn = nn.BatchNorm2d(out_planes)
+        self.relu = nn.ReLU(True)
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x3 = self.conv3(x)
+        x6 = self.conv6(x)
+        x9 = self.conv9(x)
+        x_t = x1 + (x3 + x6 + x9)/3
+        return self.relu(self.bn(x_t))
+
+
 class BatchNormReLU(nn.Module):
     def __init__(self, in_channels):
         super(BatchNormReLU, self).__init__()
