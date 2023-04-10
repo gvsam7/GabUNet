c2b3074a2f97b02bbcb99b30ed7795a010f0538a
Georgios Voulgaris <gvsam7@yahoo.com>
2023-04-10 11:56:17 +0100

    added Gabor implemented convolutional layer

--- models/ResUNet.py
+++ models/ResUNet.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from models.ConvBlock import BatchNormReLU, ResBlock, Decoder
+from models.ConvBlock import BatchNormReLU, ResBlock, Decoder, GaborConv2d
 
 
 class ResUNet(nn.Module):
@@ -9,7 +9,8 @@ class ResUNet(nn.Module):
         self.num_class = num_class
 
         # Encoder 1
-        self.conv11 = nn.Conv2d(in_channels, 64, kernel_size=3, padding=1)
+        # self.conv11 = nn.Conv2d(in_channels, 64, kernel_size=3, padding=1)
+        self.conv11 = GaborConv2d(in_channels, 64, kernel_size=3, padding=1)
         self.batchrelu = BatchNormReLU(64)
         self.conv12 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
         self.conv13 = nn.Conv2d(3, 64, kernel_size=1, padding=0)
