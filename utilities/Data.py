4751ed64b043370c0eaaaf42e0f032b5e2a61408
Georgios Voulgaris <gvsam7@yahoo.com>
2024-04-09 16:51:22 +0100

    Ensure image and mask have consistent dimensions!

--- utilities/Data.py
+++ utilities/Data.py
@@ -97,7 +97,7 @@ class Dataset(Dataset):
             image = augmentations["image"]
             mask = augmentations["mask"]
 
-            # Ensure image and mask have consistent dimensions
+            # Ensure image and mask have consistent dimensions!
             if image.shape[-2:] != mask.shape[-2:]:
                 raise ValueError("Dimensions of image and mask do not match after transformation.")
 
