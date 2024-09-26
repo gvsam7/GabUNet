7f5b473960a4eafc03b19cd540f753ad01b291cc
Georgios Voulgaris <gvsam7@yahoo.com>
2024-09-26 18:10:33 +0100

    num_heads=8

--- models/ViTResUNet.py
+++ models/ViTResUNet.py
@@ -121,7 +121,7 @@ class ViTResUNet(nn.Module):
         self.patch_embed = nn.Conv2d(256, 512, kernel_size=vit_patch_size, stride=vit_patch_size)
 
         self.transformer_encoder = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(d_model=512, nhead=10),
+            nn.TransformerEncoderLayer(d_model=512, nhead=8),
             num_layers=4
         )
 
