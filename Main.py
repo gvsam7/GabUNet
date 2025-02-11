c744e05223aeb08a4bfaf4a5b8179dd6682292fe
Georgios Voulgaris <gvsam7@yahoo.com>
2025-02-11 17:39:58 +0000

    added learning rate scheduler

--- Main.py
+++ Main.py
@@ -229,6 +229,8 @@ def main():
     print(f"criterion: {criterion}")
     # optimizer = optim.Adam(model.parameters(), lr=args.lr)
     optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-5)  # for TransUNet
+    # Initialise the scheduler
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)  # for TransUNet
 
     train_loader, val_loader, test_loader, test_ds = get_loaders(
         image_path,
@@ -284,6 +286,8 @@ def main():
 
         # check accuracy
         check_accuracy(val_loader, model, device=device, num_class=args.num_class)
+        # Update the learning rate
+        scheduler.step()  # for TransUNet
         print("Epoch:{}/{}..".format(epoch + 1, args.epochs),
               "Time: {:.2f}m".format((time.time() - since) / 60))
 
