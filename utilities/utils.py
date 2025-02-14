fdc6e905abfec1bc9882018b94e75a6a844ea2dd
Georgios Voulgaris <gvsam7@yahoo.com>
2025-02-14 12:17:04 +0000

    TreeCrowns

--- utilities/utils.py
+++ utilities/utils.py
@@ -285,7 +285,7 @@ def plot(image, mask, pred_mask, score):
     ax3.plot()
 
 
-def jaccard(pred_mask, mask, smooth=1e-10, n_classes=8):
+def jaccard(pred_mask, mask, smooth=1e-10, n_classes=16):
     with torch.no_grad():
         pred_mask = F.softmax(pred_mask, dim=1)
         pred_mask = torch.argmax(pred_mask, dim=1)
